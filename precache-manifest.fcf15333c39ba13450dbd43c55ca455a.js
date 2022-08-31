064587c6fb5c9e12afd6faea7c46bfc2ef87a298
devpurabrockers <sanjuuu2011@gmail.com>
2022-08-31 18:13:37 +0530

    Updates

---
+++ precache-manifest.fcf15333c39ba13450dbd43c55ca455a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c71bd779ee8684356f497ea163d11b04",
+    "url": "/VideoCall/index.html"
+  },
+  {
+    "revision": "061bd89fe9a951e62ec1",
+    "url": "/VideoCall/static/css/2.e1ca7e4b.chunk.css"
+  },
+  {
+    "revision": "b8fb5612c7486b31b1aa",
+    "url": "/VideoCall/static/css/main.f9dd099c.chunk.css"
+  },
+  {
+    "revision": "061bd89fe9a951e62ec1",
+    "url": "/VideoCall/static/js/2.90eef34a.chunk.js"
+  },
+  {
+    "revision": "b8fb5612c7486b31b1aa",
+    "url": "/VideoCall/static/js/main.1630c76a.chunk.js"
+  },
+  {
+    "revision": "960777038bd2a83b25b0",
+    "url": "/VideoCall/static/js/runtime~main.25162876.js"
+  },
+  {
+    "revision": "2ab67eec1a3ab14d3cc6fc3eaa072181",
+    "url": "/VideoCall/static/media/ag-index-background.2ab67eec.png"
+  },
+  {
+    "revision": "c9b3e104479e2fab5588080e81dfb689",
+    "url": "/VideoCall/static/media/ag-logo.c9b3e104.png"
+  },
+  {
+    "revision": "8504fae6a8575e0a9786f1c67bd05e69",
+    "url": "/VideoCall/static/media/icons.8504fae6.eot"
+  },
+  {
+    "revision": "bc005709a21d5a2af39b0744935ace95",
+    "url": "/VideoCall/static/media/icons.bc005709.woff"
+  },
+  {
+    "revision": "efe053d475acd04c59283fc07f718371",
+    "url": "/VideoCall/static/media/icons.efe053d4.ttf"
+  }
+]);
